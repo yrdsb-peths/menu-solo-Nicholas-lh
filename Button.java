71fa236963d0fc205b5bf9d9dac573dea31a555f
Nicholas-lh <350373023@gapps.yrdsb.ca>
2024-10-17 13:13:30 -0400

    Menu and Instruction Screen created

--- Button.java
+++ Button.java
@@ -1,3 +1,24 @@
-public class Button {
+import greenfoot.*;
 
+public class Button extends Actor
+{
+    private Runnable action;
+    
+    public Button(Runnable action)
+    {
+        this.action = action;
+        GreenfootImage image = new GreenfootImage("images/buttonLong_beige.png");
+        setImage (image);
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(this))
+        {
+            if (action != null)
+            {
+                action.run();
+            }
+        }
+    }
 }
---
+++ InstructionScreen.java
@@ -0,0 +1,11 @@
+import greenfoot. *;
+
+public class InstructionScreen extends World {
+    //maintings a reference to the MenuScreen instance that was created.
+    //menu is a variable name
+    public InstructionScreen(MenuScreen menu)
+    {
+        super(600,400,1);
+    }
+
+}
