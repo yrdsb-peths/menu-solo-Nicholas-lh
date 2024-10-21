c3cdd4387fc5cdc7ab43988bf4387347d292c64d
Nicholas-lh <350373023@gapps.yrdsb.ca>
2024-10-20 22:52:05 -0400

    instruction

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -14,8 +14,29 @@ public class InstructionScreen extends World {
         
         instructions = new String[]
         {
-            ""
-        }
+            "Welcome to the game",
+            "click to change avatars"
+        };
+        currentIndex = 0;
+        
+        instructionLabel = new Label (instructions[currentIndex], 24);
+        addObject(instructionLabel, 300, 200);
+        
+        nextButton = new Button(this::nextInstruction, "Next");
+        prevButton = new Button(this::previousInstruction, "Previous");
+        addObject(nextButton, 500, 350);
+        addObject(prevButton, 100, 350);
     }
-
+    
+    private void nextInstruction()
+    {
+        currentIndex = (currentIndex + 1) % instructions.length;
+        instructionLabel.setValue(instructions[currentIndex]);
+    }
+    
+    private void previousInstruction()
+    {
+        currentIndex = (currentIndex - 1) % instructions.length;
+        instructionLabel.setValue(instructions[currentIndex]);
+    }
 }
